1b8c12f88cdd078a671b778034a578527eb1970d
cynthiaoduol <cynthiaobu940@gmail.com>
2019-12-05 23:44:27 +0300

    cfreate url patterns for different view functions

--- epirecipe/urls.py
+++ epirecipe/urls.py
@@ -1,3 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('account/register/', views.signup, name='signup'),
+    path('new-recipe/',views.add_recipe, name='new-recipe'),
+    path('all-recipes/',views.recipes,name='food'),
+    path('single_recipe/<food_id>', views.single_recipe, name='single-recipe'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
