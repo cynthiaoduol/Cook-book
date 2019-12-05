55fc17f49a80ad34f3b530fee901a680df9503ed
cynthiaoduol <cynthiaobu940@gmail.com>
2019-12-05 23:37:41 +0300

    create the food model

--- epirecipe/models.py
+++ epirecipe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+
+class Food(models.Model):
+	name = models.CharField(max_length=100)
+	dish_image = models.ImageField(upload_to='images/', default='image.jpg')
+	ingredients = models.TextField(max_length=1000)
+	procedure = models.TextField(max_length=2000)
+	 
+	def __str__(self):
+		return self.name
+
+	def save_food(self):
+		self.save()
+
+	def delete_food(self):
+		self.delete()
