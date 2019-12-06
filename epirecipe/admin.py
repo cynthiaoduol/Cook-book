6056ab806998354bb2584f2c24133caa60ce183d
cynthiaoduol <cynthiaobu940@gmail.com>
2019-12-06 18:08:23 +0300

    register the comment class in the admin

--- epirecipe/admin.py
+++ epirecipe/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food,Profile,Comment
 
 admin.site.register(Food)
+admin.site.register(Profile)
+admin.site.register(Comment)
+
+
+
+
+
