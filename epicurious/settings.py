c7cce185d899d03242a3db0c697cddd477c46b13
cynthiaoduol <cynthiaobu940@gmail.com>
2019-12-18 18:16:31 +0300

    Fix email for subscription

--- epicurious/settings.py
+++ epicurious/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 
 # Email configurations
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Simplified static file serving.
